29627d77a06cf3f07785ad752b526330a94322e6
b_es <ivan2003064@gmail.com>
2023-05-27 02:33:09 +0300

    Что-то

---
+++ Plumbing shop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+
+namespace Plumbing_shop.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+
+        public Product(string name, string type, string description)
+        {
+            Name = name;
+            Type = type;
+            Description = description;
+        }
+    }
+}
