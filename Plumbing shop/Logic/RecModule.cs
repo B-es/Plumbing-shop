bb35d867296dcfe44ec490aca903e7676d355480
VladislavGrom <vaganov.vladislav2015@yandex.ru>
2023-06-03 17:02:27 +0300

    Добавил функцию правил + функцию сбора рекомендуемых файлов

--- Plumbing shop/Logic/RecModule.cs
+++ Plumbing shop/Logic/RecModule.cs
@@ -1,4 +1,5 @@
-﻿using Plumbing_shop.Models;
+﻿using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using Plumbing_shop.Models;
 
 namespace Plumbing_shop
 {
@@ -13,13 +14,28 @@ namespace Plumbing_shop
             dataMas.RemoveAt(0);
             List<string> values = dataMas;
             List<Entity> findEntities;
+
+            List<Entity> findOtherEnt;
+
             List<Product> Products = new List<Product>();
 
             if (entityName == "ФЧ") findEntities = db.Entities.Where(entity => entity.Name != "Труба").ToList();
             else if (entityName == string.Empty) findEntities = db.Entities.ToList();
             else findEntities = db.Entities.Where(entity => entity.Name == entityName).ToList();
 
+            findOtherEnt = db.Entities.Where(entity => entity.Name != entityName).ToList();
+
+            List<Value>? test = db.Values.Where(v => v.Id_Attribute == 3 && v.Id_Entity == 3).ToList();
+            List<string?> test2 = new List<string?>();
+            foreach (var val in test)
+            {
+                test2.Add(val.value);
+            }
+            Console.WriteLine(test2[0]);
+
             var resEntities = selectionEntities(values, findEntities);
+
+            var recEntities = getRecProducts(values, findOtherEnt);
 
             foreach (var entity in resEntities)
             {
@@ -50,6 +66,199 @@ namespace Plumbing_shop
                 }
             }
             return findEntities;
+        }
+
+        static private List<Entity> getRecProducts(List<string> values, List<Entity> findOtherEnt)
+        {
+            var ids = findOtherEnt.Select(entity => entity.Id).ToList();
+            bool сompatibility;
+            foreach (string value in values)
+            {
+                for (int i = 0; i<ids.Count; i++)
+                {
+                    сompatibility = rools(Db, value, i);
+                    if (сompatibility == false)
+                    {
+                        findOtherEnt.RemoveAt(i);
+                        ids.RemoveAt(i);
+                    }
+                    
+                }
+            }
+
+            return findOtherEnt;
+        }
+
+        static private bool rools(PlumbingDbContext db, string value, int currentId)
+        {
+            bool materialFlag = false;
+            bool tempFlag = false;
+            bool connectFlag = false;
+            bool diamFlag = false;
+
+            // Проверка совместимости по температурному режиму (Трубы) - материалу (Фасонные части)
+            if (value == "От 5 до 30 градусов" || value == "От 30 до 65 градусов" || value == "От 65 до 80 градусов" || value == "От 80 до 120 градусов")
+            {
+                // Достаем значение из БД
+                List<Value>? test = db.Values.Where(v => v.Id_Attribute == 3 && v.Id_Entity == currentId).ToList();
+                List<string?> material1 = new List<string?>();
+                foreach (var val in test)
+                {
+                    material1.Add(val.value);
+                }
+
+                string material = material1[0];
+
+                if (value == "От 5 до 30 градусов" && (material == "Сталь" || material == "Медь" || material == "Полиэтилен" || material == "ПВХ" || material == "Чугун" || material == "Бетон"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "От 30 до 65 градусов" && (material == "Сталь" || material == "Медь" || material == "Полиэтилен" || material == "ПВХ" || material == "Чугун"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "От 65 до 80 градусов" && (material == "Сталь" || material == "Медь" || material == "Полиэтилен" || material == "Чугун"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "От 80 до 120 градусов" && (material == "Сталь" || material == "Медь" || material == "Чугун"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+            }
+
+            // Проверка совместимости по материалу (Фасонные части) - температурному режиму (Трубы)
+            if (value == "Медь" || value == "Сталь" || value == "Чугун" || value == "ПВХ" || value == "Полиэтилен")
+            {
+                // Достаём значение из БД
+                List<Value>? test = db.Values.Where(v => v.Id_Attribute == 2 && v.Id_Entity == currentId).ToList();
+                List<string?> temperatureMas = new List<string?>();
+                foreach (var val in test)
+                {
+                    temperatureMas.Add(val.value);
+                }
+
+                string temperature = temperatureMas[0];
+                
+                if (value == "Медь" && (temperature == "От 5 до 30 градусов" || temperature == "От 30 до 65 градусов" || temperature == "От 65 до 80 градусов" || temperature == "От 80 до 120 градусов"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "Сталь" && (temperature == "От 5 до 30 градусов" || temperature == "От 30 до 65 градусов" || temperature == "От 65 до 80 градусов" || temperature == "От 80 до 120 градусов"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "Чугун" && (temperature == "От 5 до 30 градусов" || temperature == "От 30 до 65 градусов" || temperature == "От 65 до 80 градусов" || temperature == "От 80 до 120 градусов"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "ПВХ" && (temperature == "От 5 до 30 градусов" || temperature == "От 30 до 65 градусов"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+
+                if (value == "Полиэтилен" && (temperature == "От 5 до 30 градусов" || temperature == "От 30 до 65 градусов"))
+                {
+                    materialFlag = true;
+                }
+                else
+                {
+                    materialFlag = false;
+                }
+            }
+
+            // Проверка совместимости Типа соединения
+            if (value == "Разъёмный" || value == "Неразъёмный")
+            {
+                // Достаём значение из БД
+                List<Value>? test = db.Values.Where(v => v.Id_Attribute == 4 && v.Id_Entity == currentId).ToList();
+                List<string?> connectionMas = new List<string?>();
+                foreach (var val in test)
+                {
+                    connectionMas.Add(val.value);
+                }
+                string connection = connectionMas[0];
+
+                if (value == connection)
+                {
+                    connectFlag = true;
+                }
+                else
+                {
+                    connectFlag = false;
+                }
+            }
+
+            // Проверка совместимости по Диаметру
+            if (value == "15" || value == "20" || value == "50" || value == "80")
+            {
+                // Достаём значение из БД
+                List<Value>? test = db.Values.Where(v => v.Id_Attribute == 5 && v.Id_Entity == currentId).ToList();
+                List<string?> diameterMas = new List<string?>();
+                foreach (var val in test)
+                {
+                    diameterMas.Add(val.value);
+                }
+                string diameter = diameterMas[0];
+
+                if (value == diameter)
+                {
+                    diamFlag = true;
+                }
+                else
+                {
+                    diamFlag = false;
+                }
+            }
+
+            // Определяем подходит ли товар с учётом рассмотренных критериев совместимости
+            if (materialFlag == true && connectFlag == true && tempFlag == true && diamFlag == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
         }
     }
 }
--- Plumbing shop/Models/Constants.cs
+++ Plumbing shop/Models/Constants.cs
@@ -2,7 +2,7 @@
 {
     static public class Constants
     {
-        //static public string connectMSSQL = @"Server=localhost\SQLEXPRESS;Database=PlumbingDB;Trusted_Connection=True;TrustServerCertificate=True;";
-        static public string connectMSSQL = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ivan2\OneDrive\Документы\Test2.mdf;Integrated Security=True;Connect Timeout=30";
+        static public string connectMSSQL = @"Server=localhost\SQLEXPRESS;Database=PlumbingDB;Trusted_Connection=True;TrustServerCertificate=True;";
+        //static public string connectMSSQL = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ivan2\OneDrive\Документы\Test2.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
